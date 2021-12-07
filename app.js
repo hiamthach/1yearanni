8c8a20017b698aff331ef3d1a67fba2a647924f9
hiamthach <dreon1008@gmail.com>
2021-12-07 09:26:28 +0700

    Update code

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const firstBullet = document.querySelector('.bullets-1')
-const modal = document.querySelector('.modal')
-firstBullet.addEventListener('click', function() {
-    modal.classList.remove('display-none')
-    reduceWidth(lineAnimation, "890px")
-})
+const bullets = document.querySelectorAll('.bullets')
 const lineAnimation = document.querySelector('.line-animation')
-function reduceWidth(line, width) {
-    line.style.width = width
+const lineAniWidth = ['1050px', '890px', '650px', '490px', 0]
+for (let i = 0; i < bullets.length; i++) {
+    bullets[i].addEventListener('click', function() {
+        openModal(i+1, bullets[i])
+        
+    })
+}
+
+function openModal(index, bullet) {
+    const modal = document.querySelector(`.modal-${index}`)
+    const cardClose = modal.querySelector('.card-close')
+    modal.classList.remove('display-none')
+    cardClose.addEventListener('click', function() {
+        modal.classList.add('display-none')
+        if (!bullet.classList.contains('timeDone')) {
+            lineAnimation.style.width = lineAniWidth[index]
+            bullet.classList.add('timeDone')
+        }
+    })
 }
